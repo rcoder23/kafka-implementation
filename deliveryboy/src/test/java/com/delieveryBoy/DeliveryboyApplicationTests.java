ae3c7d863b41bbf7c5a94584949cace73dd9b30e
Rohit Srivastava <62320661+rcoder23@users.noreply.github.com>
2023-08-29 08:54:06 -0700

    Adding Kafka Codes

---
+++ deliveryboy/src/test/java/com/delieveryBoy/DeliveryboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delieveryBoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
